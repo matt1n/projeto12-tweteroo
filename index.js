317e2dd708706c73f6eb44f415235972fff56933
Mateus Macêdo <mateusamacedo5@gmail.com>
2022-11-05 17:38:20 -0300

    feat: data validation and status code

--- index.js
+++ index.js
@@ -1,47 +1,54 @@
-import express from "express"
-import cors from "cors"
+import express from "express";
+import cors from "cors";
 
-const app = express()
-app.use(express.json())
-app.use(cors())
+const app = express();
+app.use(express.json());
+app.use(cors());
 
-const serverUsers = []
-const serverTweets = []
+const serverUsers = [];
+const serverTweets = [];
 
-app.post("/sign-up", (req,res)=> {
-    const {username, avatar} = req.body
-    serverUsers.push(req.body)
-    res.send("OK")
-})
+app.post("/sign-up", (req, res) => {
+  const{username, avatar} = req.body
+    
+  if (!username || !avatar){
+    res.status(400).send("Todos os campos são obrigatórios!")
+    return
+  }
 
-app.get("/sign-up", (req,res)=> {
-    res.send(serverUsers)
-})
+  serverUsers.push(req.body);
+  res.status(201).send("OK");
+});
 
+app.post("/tweets", (req, res) => {
+    const{username, tweet} = req.body
+    
+    if (!username || !tweet){
+      res.status(400).send("Todos os campos são obrigatórios!")
+      return
+    }
+  
+    serverTweets.push(req.body);
+  res.status(201).send("OK");
+});
 
+app.get("/tweets", (req, res) => {
+  const lastTweets = serverTweets.filter(
+    (obj, i) => i >= serverTweets.length - 10 && obj
+  );
+  const lastTweetsReturn = lastTweets
+    .map(
+      (obj, i) =>
+        obj && {
+          username: obj.username,
+          avatar: serverUsers.find(
+            (objUsers) => objUsers.username === obj.username
+          ).avatar,
+          tweet: obj.tweet,
+        }
+    )
+    .reverse();
+  res.send(lastTweetsReturn);
+});
 
-app.post("/tweets", (req,res)=> {
-    serverTweets.push(req.body)
-    res.send("OK")
-})
-
-app.get("/tweets", (req,res)=> {
-    const lastTweets = serverTweets.filter((obj, i)=> i >= serverTweets.length-10 && obj)
-    const lastTweetsReturn = lastTweets.map((obj,i)=> obj && 
-    {username: obj.username,
-    avatar: serverUsers.find(objusers=> objusers.username === obj.username).avatar,
-    tweet: obj.tweet})
-    res.send(lastTweetsReturn)
-})
-
-
-// app.get("/", (req,res)=>{
-//     res.send("oi,meu nome não é julia")
-// })
-
-// app.get("/sim/:batatinhas", (req,res)=>{
-//     res.send(req.params.batatinhas)
-// })
-
-
-app.listen(5000)
+app.listen(5000);
